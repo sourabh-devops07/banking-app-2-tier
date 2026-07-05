84990ca6cc6142f5dc0f4083a94491f4f715e0b1
sourabh-devops07 <sourabhsharma0929@gmail.com>
2026-07-05 23:21:12 +0530

    smaadhan ho gya

--- smoke_test.py
+++ smoke_test.py
@@ -1,73 +1,47 @@
-import os
-import sys
-import boto3
+import boto3 ,os,sys
 import pymysql
+client=boto3.client("ssm",region_name="us-east-1")
 
-# AWS SSM Client
-client = boto3.client("ssm", region_name="us-east-1")
-
-# Fetch Parameters from Parameter Store
-response = client.get_parameters_by_path(
-    Path="/application/banking",
-    Recursive=True,
-    WithDecryption=True
-)
-
-params = {
-    os.path.basename(param["Name"]): param["Value"]
-    for param in response["Parameters"]
+params={
+    os.path.basename(p["Name"]):p["Value"]
+    for p in client.get_parameters_by_path(
+        Path="/application/banking",
+        WithDecryption=True)["Parameters"]
 }
 
-# Required Parameters
-required = [
-    "DB_HOST",
-    "DB_NAME",
-    "DB_USER",
-    "DB_PASSWORD",
-    "DB_PORT"
-]
+required=["DB_HOST","DB_NAME","DB_USER","DB_PASSWORD","DB_PORT"]
+missing=[k for k in required if k not in params]
 
-missing = [key for key in required if key not in params]
-
-print("========= Parameter Check =========")
-
-for key in required:
-    print(f"{key}: {'✅' if key in params else '❌'}")
+for k in required:
+    if k in params:
+        print(k , "✅")
+    else:
+        print(k , "❌")
 
 if missing:
-    print(f"\n❌ Missing Parameters: {missing}")
+    print(f"Failed : {missing}")
     sys.exit(1)
 
-print("\n✅ All Parameters Found\n")
 
-# Database Connection Test
+# DB FIND banking_db and show tables
 try:
-    connection = pymysql.connect(
+    connection=pymysql.connect(
         host=params["DB_HOST"],
         user=params["DB_USER"],
         password=params["DB_PASSWORD"],
-        database=params["DB_NAME"],
         port=int(params["DB_PORT"]),
         connect_timeout=10
     )
 
-    cursor = connection.cursor()
-
-    cursor.execute("SHOW TABLES")
-
-    tables = [row[0] for row in cursor.fetchall()]
-
-    print(f"Database : {params['DB_NAME']}")
-    print("Tables :")
-
-    for table in tables:
-        print(f" - {table}")
-
+    cur=connection.cursor()
+    cur.execute("SHOW TABLES")
+    tables=[row[0] for row in cur.fetchall()]
     connection.close()
+    print(f"{params["DB_NAME"]}")
+    print(f"Table : {tables}")
 
 except Exception as e:
-    print(f"\n❌ Database Connection Failed")
-    print(e)
+    print("DB ERROR ❌: ",e)
     sys.exit(1)
 
-print("\n✅ Smoke Test Passed Successfully")
+print("✅ Smoke test Done")
